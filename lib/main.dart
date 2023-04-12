29f3bc79e19a1d03d56667645f9ed0fc857d3df6
Pawin Nakvisai <pawinnak2@gmail.com>
2023-04-13 01:08:42 +0700

    Add navbar

--- lib/main.dart
+++ lib/main.dart
@@ -20,22 +20,65 @@ void main() async {
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       initialRoute: loginRoute,
-      routes: {
-        loginRoute: (context) => const LoginView(),
-        detailRoute: (context) => const DetailView(),
-        disableRoute: (context) => const DisableView(),
-        homeRoute: (context) => const HomeView(),
-        reservationRoute: (context) => const ReservationView(),
-        statusRoute: (context) => const StatusView(),
-        helpCenterRoute: (context) => const HelpCenterView(),
-        menuRoute: (context) => const MenuView(),
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case loginRoute:
+            return NoAnimationPageRoute(
+                builder: (_) => const LoginView(), settings: settings);
+          case detailRoute:
+            return NoAnimationPageRoute(
+                builder: (_) => const DetailView(), settings: settings);
+          case disableRoute:
+            return NoAnimationPageRoute(
+                builder: (_) => const DisableView(), settings: settings);
+          case homeRoute:
+            return NoAnimationPageRoute(
+                builder: (_) => const HomeView(), settings: settings);
+          case reservationRoute:
+            return NoAnimationPageRoute(
+                builder: (_) => const ReservationView(), settings: settings);
+          case statusRoute:
+            return NoAnimationPageRoute(
+                builder: (_) => const StatusView(), settings: settings);
+          case helpCenterRoute:
+            return NoAnimationPageRoute(
+                builder: (_) => const HelpCenterView(), settings: settings);
+          case menuRoute:
+            return NoAnimationPageRoute(
+                builder: (_) => const MenuView(), settings: settings);
+          default:
+            return null;
+        }
       },
     );
+  }
+}
+
+// Route that removes the sliding animation for specific routes
+class NoAnimationPageRoute<T> extends MaterialPageRoute<T> {
+  final List<String> excludedRoutes = [
+    homeRoute,
+    statusRoute,
+    menuRoute,
+  ];
+
+  NoAnimationPageRoute(
+      {required WidgetBuilder builder, required RouteSettings settings})
+      : super(builder: builder, settings: settings);
+
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) {
+    if (excludedRoutes.contains(settings.name)) {
+      return child;
+    }
+    return super
+        .buildTransitions(context, animation, secondaryAnimation, child);
   }
 }
