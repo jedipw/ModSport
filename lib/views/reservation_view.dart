9f4a657bcbfe306483f0145906a214c5c0ec152e
Pawin Nakvisai <pawinnak2@gmail.com>
2023-04-16 22:27:13 +0700

    Reservation Success! modal

--- lib/views/reservation_view.dart
+++ lib/views/reservation_view.dart
@@ -304,6 +304,57 @@ class _ReservationViewState extends State<ReservationView> {
                                   setState(() {
                                     _isReserved = true;
                                   });
+                                  showDialog(
+                                    barrierDismissible: false,
+                                    context: context,
+                                    barrierColor: Colors.white.withOpacity(0.5),
+                                    builder: (BuildContext context) {
+                                      return Center(
+                                        child: AlertDialog(
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10.0),
+                                          ),
+                                          contentPadding: EdgeInsets.zero,
+                                          content: Column(
+                                            mainAxisSize: MainAxisSize.min,
+                                            children: [
+                                              const SizedBox(height: 60),
+                                              Container(
+                                                width: 100,
+                                                height: 100,
+                                                decoration: const BoxDecoration(
+                                                  shape: BoxShape.circle,
+                                                  color: Colors.green,
+                                                ),
+                                                child: const Icon(Icons.check,
+                                                    color: Colors.white,
+                                                    size: 80),
+                                              ),
+                                              const SizedBox(height: 10),
+                                              const Text(
+                                                'Success!',
+                                                style: TextStyle(
+                                                  fontSize: 20.0,
+                                                  fontWeight: FontWeight.bold,
+                                                  fontFamily: 'Poppins',
+                                                  color: Colors.black,
+                                                  height: 1.3,
+                                                  letterSpacing: 0.0,
+                                                ),
+                                                textAlign: TextAlign.center,
+                                              ),
+                                              const SizedBox(height: 60),
+                                            ],
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                  );
+                                  Future.delayed(const Duration(seconds: 1),
+                                      () {
+                                    Navigator.of(context).pop();
+                                  });
                                 }
                               },
                             ),
