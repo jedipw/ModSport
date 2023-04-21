26248a1e1457e934cf09cbacf43724ae5de1bd64
Pawin Nakvisai <pawinnak2@gmail.com>
2023-04-21 20:28:44 +0700

    Change app bar insome pages

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -10,37 +10,117 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   final int _currentDrawerIndex =
       0; // Define current selected index of the drawer
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: ModSportDrawer(currentDrawerIndex: _currentDrawerIndex),
-        appBar: AppBar(
-          title: const Text('Home'),
-          backgroundColor: const Color.fromARGB(255, 225, 115, 37),
-        ),
-        body: Center(
-            child: TextButton(
-          style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(
-                  Colors.orange)), // Set the button background color
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) =>
-                    const ReservationView(zoneId: 'TEHMJQbzdGplLBfcrZq0'),
-              ),
-            );
-          },
-          child: const Text(
-            "Badminton Court 1",
-            style: TextStyle(
-              color: Colors.white,
+      key: _scaffoldKey,
+      drawer: ModSportDrawer(currentDrawerIndex: _currentDrawerIndex),
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(height: 230),
+                // Start writing your code here
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(Colors
+                              .orange)), // Set the button background color
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const ReservationView(
+                                zoneId: 'TEHMJQbzdGplLBfcrZq0'),
+                          ),
+                        );
+                      },
+                      child: const Text(
+                        "Badminton Court 1",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
-        )));
+          Stack(
+            children: [
+              Container(
+                height: 200,
+                decoration: const BoxDecoration(
+                  color: Color(0xFFE17325),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20.0),
+                    bottomRight: Radius.circular(20.0),
+                  ),
+                ),
+                padding: const EdgeInsets.only(bottom: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: const [
+                        Text(
+                          'KMUTT RESERVATION',
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 24.0,
+                            height: 1.5,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 35),
+                    Container(
+                        padding: const EdgeInsets.fromLTRB(35, 0, 35, 0),
+                        height: 40,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                              borderSide: BorderSide.none,
+                            ),
+                          ),
+                        ))
+                  ],
+                ),
+              ),
+              Positioned(
+                left: 15,
+                top: 65,
+                child: IconButton(
+                  onPressed: () {
+                    if (_scaffoldKey.currentState?.isDrawerOpen ?? false) {
+                      _scaffoldKey.currentState?.openEndDrawer();
+                    } else {
+                      _scaffoldKey.currentState?.openDrawer();
+                    }
+                  },
+                  icon: const Icon(Icons.menu, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/views/status_view.dart
+++ lib/views/status_view.dart
@@ -11,37 +11,99 @@ class StatusView extends StatefulWidget {
 
 class _StatusViewState extends State<StatusView> {
   final int _currentDrawerIndex = 1;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       drawer: ModSportDrawer(currentDrawerIndex: _currentDrawerIndex),
-      appBar: AppBar(
-        title: const Text('Status'),
-        backgroundColor: const Color.fromARGB(255, 225, 115, 37),
-      ), // add the app bar
-      body: Center(
-        child: TextButton(
-          style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(
-                  Colors.orange)), // set button style with background color
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => DetailView(
-                    zoneId: '54321',
-                    startDateTime: DateTime(2023, 4, 17, 8, 0, 0)),
-              ),
-            ); // navigate to the detailed status page when button is pressed
-          },
-          child: const Text(
-            'Detailed status (ID: 54321, StartTime: 17 April 08:00:00)',
-            style: TextStyle(
-              color: Colors.white,
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(height: 150),
+                // Start writing your code here
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Center(
+                      child: TextButton(
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(Colors
+                                .orange)), // set button style with background color
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => DetailView(
+                                  zoneId: '54321',
+                                  startDateTime:
+                                      DateTime(2023, 4, 17, 8, 0, 0)),
+                            ),
+                          ); // navigate to the detailed status page when button is pressed
+                        },
+                        child: const Text(
+                          'Detailed status (ID: 54321, StartTime: 17 April 08:00:00)',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
-        ),
+          Stack(
+            children: [
+              Container(
+                height: 125,
+                decoration: const BoxDecoration(
+                  color: Color(0xFFE17325),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20.0),
+                    bottomRight: Radius.circular(20.0),
+                  ),
+                ),
+                padding: const EdgeInsets.only(bottom: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: const [
+                    Text(
+                      'STATUS',
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 24.0,
+                        height: 1.5,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                left: 15,
+                top: 65,
+                child: IconButton(
+                  onPressed: () {
+                    if (_scaffoldKey.currentState?.isDrawerOpen ?? false) {
+                      _scaffoldKey.currentState?.openEndDrawer();
+                    } else {
+                      _scaffoldKey.currentState?.openDrawer();
+                    }
+                  },
+                  icon: const Icon(Icons.menu, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
