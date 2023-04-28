9938824b9e8c8c9db182098e375e09534c44e738
Pawin Nakvisai <pawinnak2@gmail.com>
2023-04-28 18:03:49 +0700

    Update iphone text header

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:modsport/constants/color.dart';
 import 'package:modsport/utilities/drawer.dart';
 import 'package:modsport/views/reservation_view.dart';
@@ -18,6 +22,21 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      onDrawerChanged: (isOpened) {
+        isOpened && Platform.isIOS
+            ? SystemChrome.setSystemUIOverlayStyle(
+                SystemUiOverlayStyle.dark.copyWith(
+                statusBarColor:
+                    primaryGray, // set to Colors.black for black color
+              ))
+            : Platform.isIOS
+                ? SystemChrome.setSystemUIOverlayStyle(
+                    SystemUiOverlayStyle.light.copyWith(
+                    statusBarColor:
+                        primaryGray, // set to Colors.black for black color
+                  ))
+                : null;
+      },
       key: _scaffoldKey,
       drawer: ModSportDrawer(currentDrawerIndex: _currentDrawerIndex),
       body: Stack(
