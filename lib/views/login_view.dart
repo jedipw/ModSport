030311943d48bcbb1599dc189637c19d32f88046
Thitipa <thitipanet@gmail.com>
2023-04-29 23:51:29 +0700

    Start make the auth with email in register page

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -10,13 +10,46 @@ class LoginView extends StatelessWidget {
   Widget build(BuildContext context) {
     // required method to build the UI
     return Scaffold(
-      // scaffold widget provides a basic app bar, drawer and body
-      appBar: AppBar(
-        title: const Text('Login'),
-        backgroundColor: primaryOrange,
-      ), // app bar with a title
-      body: Row(
+        body: Column(children: [
+      Padding(
+        padding: const EdgeInsets.fromLTRB(40, 90, 0, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Image.asset(
+                  'assets/images/modsportlogoorange.png',
+                  scale: 25,
+                ),
+                const Text(
+                  "LOG IN",
+                  style: TextStyle(
+                    color: primaryOrange,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Poppins",
+                  ),
+                ),
+              ],
+            ),
+            const Text(
+              "Please sign in to continue",
+              style: TextStyle(
+                fontFamily: "Poppins",
+                fontSize: 16,
+                height: 1.5,
+                color: primaryGray,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+      const SizedBox(height: 150),
+      Row(
         mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -104,6 +137,174 @@ class LoginView extends StatelessWidget {
                 ),
               ),
             ],
+          ),
+        ],
+      ),
+    ]));
+
+    // Navigation to other 4 pages
+    // Row(
+    //   mainAxisAlignment: MainAxisAlignment.center,
+    //   children: [
+    //     Column(
+    //       mainAxisAlignment: MainAxisAlignment.center,
+    //       children: [
+    //         TextButton(
+    //           // a flat button with a text label
+    //           style: ButtonStyle(
+    //               // style the button
+    //               backgroundColor: MaterialStateProperty.all(
+    //                   primaryOrange)), // set button background color
+    //           onPressed: () {
+    //             // method called when button is pressed
+    //             Navigator.of(context).pushNamed(
+    //               registerRoute,
+    //             );
+    //           },
+    //           child: const Text(
+    //             // label text for the button
+    //             "Register",
+    //             style: TextStyle(
+    //               color: Colors.white,
+    //             ),
+    //           ),
+    //         ),
+    //         TextButton(
+    //           // a flat button with a text label
+    //           style: ButtonStyle(
+    //               // style the button
+    //               backgroundColor: MaterialStateProperty.all(
+    //                   primaryOrange)), // set button background color
+    //           onPressed: () {
+    //             // method called when button is pressed
+    //             Navigator.of(context).pushNamed(
+    //               forgetPasswordRoute,
+    //             );
+    //           },
+    //           child: const Text(
+    //             // label text for the button
+    //             "Forget Password",
+    //             style: TextStyle(
+    //               color: Colors.white,
+    //             ),
+    //           ),
+    //         ),
+    //         TextButton(
+    //           // a flat button with a text label
+    //           style: ButtonStyle(
+    //               // style the button
+    //               backgroundColor: MaterialStateProperty.all(
+    //                   primaryOrange)), // set button background color
+    //           onPressed: () {
+    //             // method called when button is pressed
+    //             Navigator.of(context).pushNamed(
+    //               verifyEmailRoute,
+    //             );
+    //           },
+    //           child: const Text(
+    //             // label text for the button
+    //             "Login with unverified email!",
+    //             style: TextStyle(
+    //               color: Colors.white,
+    //             ),
+    //           ),
+    //         ),
+    //         TextButton(
+    //           // a flat button with a text label
+    //           style: ButtonStyle(
+    //               // style the button
+    //               backgroundColor: MaterialStateProperty.all(
+    //                   primaryOrange)), // set button background color
+    //           onPressed: () {
+    //             // method called when button is pressed
+    //             Navigator.of(context).pushNamedAndRemoveUntil(
+    //               // navigates to homeRoute screen and removes previous routes
+    //               homeRoute,
+    //               (route) => false,
+    //             );
+    //           },
+    //           child: const Text(
+    //             // label text for the button
+    //             "Login with verified email!",
+    //             style: TextStyle(
+    //               color: Colors.white,
+    //             ),
+    //           ),
+    //         ),
+    //       ],
+    //     ),
+    //   ],
+
+    // ])
+  }
+}
+
+class LoginForm extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  LoginForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 32),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  blurRadius: 10,
+                  offset: const Offset(0, 5),
+                ),
+              ],
+            ),
+            child: TextFormField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                hintText: "Email",
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.symmetric(horizontal: 16),
+              ),
+            ),
+          ),
+          const SizedBox(height: 16),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  blurRadius: 10,
+                  offset: Offset(0, 5),
+                ),
+              ],
+            ),
+            child: TextFormField(
+              controller: passwordController,
+              decoration: const InputDecoration(
+                hintText: "Password",
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.symmetric(horizontal: 16),
+              ),
+              obscureText: true,
+            ),
+          ),
+          const SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: primaryOrange,
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            child: const Text("Log in"),
           ),
         ],
       ),
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,44 +1,416 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:modsport/constants/color.dart';
 import 'package:modsport/constants/routes.dart';
+import 'package:modsport/utilities/modal.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modsport/firebase_options.dart';
 
 class RegisterView extends StatelessWidget {
-  const RegisterView(
-      {super.key}); // constructor for LoginView, takes an optional key parameter
+  static final _formKey = GlobalKey<FormState>();
+  static TextEditingController FnameController = TextEditingController();
+  static TextEditingController LnameController = TextEditingController();
+  static TextEditingController emailController = TextEditingController();
+  static TextEditingController passwordController = TextEditingController();
+  static TextEditingController confirmPasswordController =
+      TextEditingController();
+
+  // constructor for LoginView, takes an optional key parameter
+  const RegisterView({
+    Key? key,
+  }) : super(key: key);
+  // String get email => emailController.text.trim();
+  // String get password => passwordController.text.trim();
+
+  // Confirm password validation method
+  String? _validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your confirm password';
+    }
+    if (value.length < 6) {
+      return 'Password should be at least 6 characters';
+    }
+    if (value != passwordController.text) {
+      return 'Passwords do not match';
+    }
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
     // required method to build the UI
     return Scaffold(
-      // scaffold widget provides a basic app bar, drawer and body
-      appBar: AppBar(
-        title: const Text('Register'),
-        backgroundColor: primaryOrange,
-      ), // app bar with a title
-      body: Center(
-        // centers child widget in the screen
-        child: TextButton(
-          // a flat button with a text label
-          style: ButtonStyle(
-              // style the button
-              backgroundColor: MaterialStateProperty.all(
-                  primaryOrange)), // set button background color
-          onPressed: () {
-            // method called when button is pressed
-            Navigator.of(context).pushNamed(
-              // navigates to homeRoute screen and removes previous routes
-              verifyEmailRoute,
-            );
+        // scaffold widget provides a basic app bar, drawer and body
+        appBar: AppBar(
+          title: const Text('Register'),
+          backgroundColor: primaryOrange,
+        ), // app bar with a title
+        body: FutureBuilder(
+          future: Firebase.initializeApp(
+              options: DefaultFirebaseOptions.currentPlatform),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                return Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      //Name
+                      TextFormField(
+                        controller: FnameController,
+                        decoration: const InputDecoration(labelText: "Name"),
+                        validator: (value) {
+                          if (value == null ||
+                              value.isEmpty ||
+                              !RegExp(r'^[a-zA-Z- ]+$').hasMatch(value)) {
+                            return 'Please enter your name (only as a character and - allowed).';
+                          }
+                          return null;
+                        },
+                      ),
+                      //Surname
+                      TextFormField(
+                        controller: LnameController,
+                        decoration: const InputDecoration(labelText: "Surname"),
+                        validator: (value) {
+                          if (value == null ||
+                              value.isEmpty ||
+                              !RegExp(r'^[a-zA-Z- ]+$').hasMatch(value)) {
+                            return 'Please enter your Surname (only as a character and - allowed).';
+                          }
+                          return null;
+                        },
+                      ),
+                      //Email
+                      TextFormField(
+                        controller: emailController,
+                        decoration: const InputDecoration(
+                            labelText: "Email",
+                            hintText: "user12345@kmutt.ac.th"),
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your email address';
+                          }
+                          if (!RegExp(
+                                  r'^[\w-\.]+@(kmutt\.ac\.th|mail\.kmutt\.ac\.th)$')
+                              .hasMatch(value)) {
+                            return 'Please enter a valid KMUTT email address';
+                          }
+                          return null;
+                        },
+                      ),
+
+                      //Password
+                      TextFormField(
+                        controller: passwordController,
+                        decoration:
+                            const InputDecoration(labelText: "Password"),
+                        obscureText: true,
+                        enableSuggestions: false,
+                        autocorrect: false,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your password';
+                          }
+                          if (value.length < 6) {
+                            return 'Password should be at least 6 characters';
+                          }
+                          return null;
+                        },
+                      ),
+                      //Confirm Password
+                      TextFormField(
+                        controller: confirmPasswordController,
+                        decoration: const InputDecoration(
+                            labelText: "Confirm Password"),
+                        obscureText: true,
+                        enableSuggestions: false,
+                        autocorrect: false,
+                        validator: _validatePassword,
+                      ),
+                      const SizedBox(height: 25),
+                      Center(
+                        // centers child widget in the screen
+                        child: TextButton(
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(primaryOrange),
+                            shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(40))),
+                            minimumSize: MaterialStateProperty.all(
+                                const Size(173.42, 64)),
+                          ),
+                          onPressed: () async {
+                            // if (_formKey.currentState!.validate()) {
+                            //   final email = emailController.text;
+                            //   final password = passwordController.text;
+                            //   final userCredential = await FirebaseAuth.instance
+                            //       .createUserWithEmailAndPassword(
+                            //           email: email, password: password);
+                            //   print(userCredential);
+                            // }
+                            if (_formKey.currentState!.validate()) {
+                              showAccountConfirmationModal(
+                                context,
+                                () async {
+                                  try {
+                                    final email = emailController.text;
+                                    final password = passwordController.text;
+                                    Navigator.of(context).pop();
+                                    showLoadModal(context);
+                                    final userCredential = await FirebaseAuth
+                                        .instance
+                                        .createUserWithEmailAndPassword(
+                                            email: email, password: password);
+                                    Navigator.of(context).pop();
+                                    Navigator.of(context).pushNamed(
+                                      //navigates to homeRoute screen and removes previous routes
+                                      verifyEmailRoute,
+                                    );
+                                  } catch (e) {
+                                    showErrorModal(
+                                      context,
+                                      () {
+                                        Navigator.of(context).pop();
+                                        Navigator.of(context).pop();
+                                      },
+                                    );
+                                    // throw(e);
+                                  }
+                                },
+                                FnameController.text,
+                                LnameController.text,
+                                emailController.text,
+                              );
+                            }
+                          },
+                          child: const Text(
+                            // label text for the button
+                            "Sign up",
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w500,
+                              fontSize: 24.0,
+                              height: 1.0,
+                              color: Colors.white,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              default:
+                return const Text("Loading...");
+            }
           },
-          child: const Text(
-            // label text for the button
-            "Register!",
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-        ),
-      ),
-    );
+        ));
   }
 }
+
+// Custom Modal
+dynamic showAccountConfirmationModal(BuildContext context,
+    OnPressedCallBack onPressed, String Fname, String Lname, String email) {
+  showDialog(
+    context: context,
+    barrierColor: Colors.white.withOpacity(0.5),
+    builder: (BuildContext context) {
+      return AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        contentPadding: EdgeInsets.zero,
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Column(
+              children: [
+                const SizedBox(height: 40),
+                Text(
+                  'Do you want to use\n$Fname $Lname\nas your name and \n$email\nas email ?',
+                  style: const TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Poppins',
+                    color: primaryRed,
+                    height: 1.3,
+                    letterSpacing: 0.0,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 30),
+              ],
+            ),
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 84,
+                      height: 43,
+                      child: TextButton(
+                        onPressed: onPressed,
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                            primaryGreen,
+                          ),
+                          foregroundColor: MaterialStateProperty.all<Color>(
+                            Colors.white,
+                          ),
+                        ),
+                        child: const Text(
+                          'Yes',
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 20.0,
+                            height: 1.2,
+                            letterSpacing: 0.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 34.0),
+                    SizedBox(
+                      width: 84,
+                      height: 43,
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                            primaryRed,
+                          ),
+                          foregroundColor: MaterialStateProperty.all<Color>(
+                            Colors.white,
+                          ),
+                        ),
+                        child: const Text(
+                          'No',
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 20.0,
+                            height: 1.2,
+                            letterSpacing: 0.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 30),
+          ],
+        ),
+      );
+    },
+  );
+}
+
+///////////////////// The nav part put in column/////////////////////
+// child: Column(
+//   children: [
+//     //Name
+//     const TextField(
+//       decoration: InputDecoration(hintText: "Name"),
+//     ),
+//     //Surname
+//     const TextField(
+//       decoration: InputDecoration(hintText: "Surname"),
+//     ),
+//     //Email
+//     TextField(
+//       controller: emailController,
+//       decoration:
+//           const InputDecoration(hintText: "user12345@kmutt.ac.th"),
+//       keyboardType: TextInputType.emailAddress,
+//     ),
+
+//     //Password
+//     TextField(
+//       controller: passwordController,
+//       decoration: const InputDecoration(hintText: "Password"),
+//       obscureText: true,
+//       enableSuggestions: false,
+//       autocorrect: false,
+//     ),
+//     //Confirm Password
+//     TextField(
+//       controller: passwordController,
+//       decoration: const InputDecoration(hintText: "Confirm Password"),
+//       obscureText: true,
+//       enableSuggestions: false,
+//       autocorrect: false,
+//     ),
+//     const SizedBox(height: 25),
+//     Center(
+//       // centers child widget in the screen
+//       child: TextButton(
+//         style: ButtonStyle(
+//           backgroundColor: MaterialStateProperty.all(primaryOrange),
+//           shape: MaterialStateProperty.all(RoundedRectangleBorder(
+//               borderRadius: BorderRadius.circular(40))),
+//           minimumSize:
+//               MaterialStateProperty.all(const Size(173.42, 64)),
+//         ),
+//         onPressed: () async {
+//           await Firebase.initializeApp(
+//               options: DefaultFirebaseOptions.currentPlatform
+//           );
+
+//           final email = emailController.text;
+//           final password = passwordController.text;
+//           final userCredential = await FirebaseAuth.instance
+//               .createUserWithEmailAndPassword(
+//                   email: email, password: password);
+//           print(userCredential);
+//         },
+//         child: const Text(
+//           // label text for the button
+//           "Sign up",
+//           style: TextStyle(
+//             fontFamily: 'Poppins',
+//             fontWeight: FontWeight.w500,
+//             fontSize: 24.0,
+//             height: 1.0,
+//             color: Colors.white,
+//           ),
+//           textAlign: TextAlign.center,
+//         ),
+//       ),
+//     ),
+//     //Nav to verify page
+//     // TextButton(
+//     //   // a flat button with a text label
+//     //   style: ButtonStyle(
+//     //       // style the button
+//     //       backgroundColor: MaterialStateProperty.all(
+//     //           primaryOrange)), // set button background color
+//     //   onPressed: () {
+//     //     // method called when button is pressed
+//     //     Navigator.of(context).pushNamed(
+//     //       // navigates to homeRoute screen and removes previous routes
+//     //       verifyEmailRoute,
+//     //     );
+//     //   },
+//     //   child: const Text(
+//     //     // label text for the button
+//     //     "Register!",
+//     //     style: TextStyle(
+//     //       color: Colors.white,
+//     //     ),
+//     //   ),
+//     // ),
+//   ],
+// ),
