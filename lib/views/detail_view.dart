1fe96b75a3f27f29ad65d8bb6a678218a99afa80
Dream_Winanya <winanya2545@gmail.com>
2023-05-02 01:08:07 +0700

    Update detail_view.dart
    stack กลับหัว ลงไปข้างล่าง + ของ disabled ยังไม่เขียน

--- lib/views/detail_view.dart
+++ lib/views/detail_view.dart
@@ -49,8 +49,23 @@ class _DetaiViewState extends State<DetailView> {
               DateFormat('d MMM y').format(startDateTime); // Example date format
           final formattedTime =
               DateFormat('HH:mm').format(startDateTime); // Example time format
-          final bool isSuccessful = data['isSuccessful'] ?? false;
+          final bool isSuccessful = data['isSuccessful'] ?? true;
           final String? disableReason = data['disableReason'];
+
+          return StreamBuilder<DocumentSnapshot>(
+                  stream: FirebaseFirestore.instance
+                    .collection('zone')
+                    .doc(widget.zoneId)
+                    .snapshots(),
+                  builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+                    if (!snapshot.hasData) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+
+                    final zoneData = snapshot.data!.data() as Map<String, dynamic>;
+                    final String zoneName = zoneData['zoneName'];
 
           return Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                             children: [
@@ -66,8 +81,21 @@ class _DetaiViewState extends State<DetailView> {
                                     fontWeight: FontWeight.w400,
                                     fontSize: 12.0,
                                     height: 1.5,),),
-              Text('Date: $formattedDate'),
-              Text('Time: $formattedTime'),
+                              Text('Facility: $zoneName', style: TextStyle(fontFamily: 'Poppins',
+                                    fontStyle: FontStyle.normal,
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 12.0,
+                                    height: 1.5,),),
+                              Text('Date: $formattedDate', style: TextStyle(fontFamily: 'Poppins',
+                                    fontStyle: FontStyle.normal,
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 12.0,
+                                    height: 1.5,),),
+                              Text('Time: $formattedTime', style: TextStyle(fontFamily: 'Poppins',
+                                    fontStyle: FontStyle.normal,
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 12.0,
+                                    height: 1.5,),),
               if (!isSuccessful && disableReason != null)
                 StreamBuilder<QuerySnapshot>(
                   stream: FirebaseFirestore.instance
@@ -88,54 +116,51 @@ class _DetaiViewState extends State<DetailView> {
                     return Text('due to $disableReason');
                   },
                 ),
-                Stack(
-                children: [
-                  Container(
-                    height: 125,
-                    decoration: const BoxDecoration(
-                      color: primaryOrange,
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(20.0),
-                        bottomRight: Radius.circular(20.0),
-                      ),
-                    ),
-                    padding: const EdgeInsets.only(bottom: 17),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: const [
-                        Text(
-                          'STATUS',
-                          style: TextStyle(
-                            fontFamily: 'Poppins',
-                            fontStyle: FontStyle.normal,
-                            fontWeight: FontWeight.w600,
-                            fontSize: 24.0,
-                            height: 1.5,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
+            Stack(
+            children: [
+              Container(
+                height: 125,
+                decoration: const BoxDecoration(
+                  color: primaryOrange,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20.0),
+                    bottomRight: Radius.circular(20.0),
                   ),
-                  Positioned(
-                    left: 5,
-                    top: 65,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        if (_scaffoldKey.currentState?.isDrawerOpen ?? false) {
-                          _scaffoldKey.currentState?.openEndDrawer();
-                        } else {
-                          _scaffoldKey.currentState?.openDrawer();
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: primaryOrange,
-                        shape: const CircleBorder(),
-                        fixedSize: const Size.fromRadius(25),
-                        elevation: 0,
+                ),
+                padding: const EdgeInsets.only(bottom: 17),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: const [
+                    Text(
+                      'STATUS',
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 24.0,
+                        height: 1.5,
+                        color: Colors.white,
                       ),
-                      child: const Icon(Icons.menu, color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                left: 5,
+                top: 65,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: primaryOrange,
+                    shape: const CircleBorder(),
+                    padding: const EdgeInsets.fromLTRB(12, 4, 4, 4),
+                    fixedSize: const Size.fromRadius(25),
+                    elevation: 0,
+                  ),
+                  child: const Icon(Icons.arrow_back_ios, color: Colors.white),
                     ),
                   ),
                 ],
@@ -144,7 +169,7 @@ class _DetaiViewState extends State<DetailView> {
             
           );
         },
-      ),
-    );
+      );
+    }));
   }
 }
