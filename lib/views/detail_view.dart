0a307218237493e8afef5da29c93b6c47976d024
Dream_Winanya <winanya2545@gmail.com>
2023-05-16 21:36:25 +0700

    Update detail_view.dart

--- lib/views/detail_view.dart
+++ lib/views/detail_view.dart
@@ -1,10 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 import 'package:modsport/constants/color.dart';
 
 import 'package:modsport/services/cloud/firebase_cloud_storage.dart';
 import 'package:modsport/utilities/drawer.dart';
-import 'package:modsport/utilities/reservation/location_name.dart';
 import 'package:modsport/utilities/types.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
@@ -12,16 +14,17 @@ import 'package:shimmer/shimmer.dart';
 
 class DetailView extends StatefulWidget {
   final String zoneId;
-  const DetailView({Key? key, required this.zoneId, required DateTime startDateTime}) : super(key: key);
+  final DateTime startDateTime;
+
+  const DetailView(
+      {Key? key, required this.zoneId, required this.startDateTime})
+      : super(key: key);
 
   @override
   State<DetailView> createState() => _DetaiViewState();
 }
 
 class _DetaiViewState extends State<DetailView> {
-  final String userId = FirebaseAuth.instance.currentUser!.uid;
-
-
   final int _currentDrawerIndex = 1;
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
@@ -183,6 +186,14 @@ class _DetaiViewState extends State<DetailView> {
 
   @override
   Widget build(BuildContext context) {
+    Platform.isIOS
+        ? SystemChrome.setSystemUIOverlayStyle(
+            SystemUiOverlayStyle.light.copyWith(
+              statusBarColor:
+                  Colors.white, // set to Colors.black for black color
+            ),
+          )
+        : null;
     final formattedTime = DateFormat('HH:mm').format(widget.startDateTime);
     final formattedEndTime = DateFormat('HH:mm').format(endTime);
 
@@ -637,199 +648,3 @@ class _DetaiViewState extends State<DetailView> {
     );
   }
 }
-  //     body: StreamBuilder<QuerySnapshot>(
-  //       stream: FirebaseFirestore.instance
-  //                 .collection('userreservation')
-  //                 .where('userId', isEqualTo: userId)
-  //                 .where('zoneId', isEqualTo: widget.zoneId)
-  //                 .where('locationId', isEqualTo: widget.locationId)
-  //                 .snapshots(),
-  //             builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-  //               if (!snapshot.hasData) {
-  //                 return const Center(
-  //                   child: CircularProgressIndicator(),
-  //                 );
-  //               }
-
-  //               final data = snapshot.data!.docs.first.data() as Map<String, dynamic>;
-  //               final startDateTime = widget.startDateTime;
-  //               final formattedDate = DateFormat('d MMM y').format(startDateTime);
-  //               final formattedTime = DateFormat('HH:mm').format(startDateTime);
-  //               final bool isSuccessful = data['isSuccessful'] ?? true;
-  //               final String? disableReason = data['disableReason'];
-
-  //               return StreamBuilder<DocumentSnapshot>(
-  //                 stream: FirebaseFirestore.instance.collection('zone').doc(widget.zoneId).snapshots(),
-  //                 builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-  //                   if (!snapshot.hasData) {
-  //                     return const Center(
-  //                       child: CircularProgressIndicator(),
-  //                     );
-  //                   }
-
-  //                   final zoneData = snapshot.data!.data() as Map<String, dynamic>;
-  //                   final String zoneName = zoneData['zoneName'];
-
-  //                   return StreamBuilder<DocumentSnapshot>(
-  //                     stream: FirebaseFirestore.instance.collection('location').doc(widget.locationId).snapshots(),
-  //                     builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-  //                       if (!snapshot.hasData) {
-  //                         return const Center(
-  //                           child: CircularProgressIndicator(),
-  //                         );
-  //                       }
-
-  //                       final locationData = snapshot.data!.data() as Map<String, dynamic>;
-  //                       final String locationName = locationData['locationName'];
-
-  //         return Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-  //                           children: [
-  //                             Icon(Icons.check_circle,color: Colors.green,size: 150.0,),
-  //                             Text('Successful', style: TextStyle(fontFamily: 'Poppins',
-  //                                   fontStyle: FontStyle.normal,
-  //                                   fontWeight: FontWeight.w500,
-  //                                   fontSize: 24.0,
-  //                                   height: 1.5,
-  //                                   color: Colors.green,),),
-  //                             Text('Your reservation has been successful!', style: TextStyle(fontFamily: 'Poppins',
-  //                                   fontStyle: FontStyle.normal,
-  //                                   fontWeight: FontWeight.w400,
-  //                                   fontSize: 12.0,
-  //                                   height: 1.5,),),
-  //                             Text('Facility: $zoneName', style: TextStyle(fontFamily: 'Poppins',
-  //                                   fontStyle: FontStyle.normal,
-  //                                   fontWeight: FontWeight.w400,
-  //                                   fontSize: 12.0,
-  //                                   height: 1.5,),),
-  //                             Text('Location: $locationName', style: TextStyle(fontFamily: 'Poppins',
-  //                                   fontStyle: FontStyle.normal,
-  //                                   fontWeight: FontWeight.w400,
-  //                                   fontSize: 12.0,
-  //                                   height: 1.5,),),  
-  //                             Text('Date: $formattedDate', style: TextStyle(fontFamily: 'Poppins',
-  //                                   fontStyle: FontStyle.normal,
-  //                                   fontWeight: FontWeight.w400,
-  //                                   fontSize: 12.0,
-  //                                   height: 1.5,),),
-  //                             Text('Time: $formattedTime', style: TextStyle(fontFamily: 'Poppins',
-  //                                   fontStyle: FontStyle.normal,
-  //                                   fontWeight: FontWeight.w400,
-  //                                   fontSize: 12.0,
-  //                                   height: 1.5,),),
-
-
-  //             if (!isSuccessful && disableReason != null)
-  //               StreamBuilder<QuerySnapshot>(
-  //                 stream: FirebaseFirestore.instance
-  //                     .collection('disable')
-  //                     .where('userId', isEqualTo: userId)
-  //                     .where('zoneId', isEqualTo: widget.zoneId)
-  //                     .where('locationId', isEqualTo: widget.locationId)
-  //                     .snapshots(),
-  //                 builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-  //                   if (!snapshot.hasData) {
-  //                     return const Center(
-  //                       child: CircularProgressIndicator(),
-  //                     );
-  //                   }
-
-  //                                 final disableData = snapshot.data!.docs.first
-  //                                     .data() as Map<String, dynamic>;
-  //                                 final String disableReason =
-  //                                     disableData['disableReason'];
-
-  //                   return Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, 
-  //                           children: [
-  //                             Icon(Icons.cancel_rounded,color: Colors.red,size: 150.0,),
-  //                             Text('Canceled', style: TextStyle(fontFamily: 'Poppins',
-  //                                   fontStyle: FontStyle.normal,
-  //                                   fontWeight: FontWeight.w500,
-  //                                   fontSize: 24.0,
-  //                                   height: 1.5,
-  //                                   color: Colors.red,),),
-  //                             Text('Reason: $disableReason',style: TextStyle(fontFamily: 'Poppins',
-  //                                   fontStyle: FontStyle.normal,
-  //                                   fontWeight: FontWeight.w400,
-  //                                   fontSize: 12.0,
-  //                                   height: 1.5,),),
-  //                             Text('Facility: $zoneName', style: TextStyle(fontFamily: 'Poppins',
-  //                                   fontStyle: FontStyle.normal,
-  //                                   fontWeight: FontWeight.w400,
-  //                                   fontSize: 12.0,
-  //                                   height: 1.5,),),
-  //                             Text('Location: $locationName', style: TextStyle(fontFamily: 'Poppins',
-  //                                   fontStyle: FontStyle.normal,
-  //                                   fontWeight: FontWeight.w400,
-  //                                   fontSize: 12.0,
-  //                                   height: 1.5,),),  
-  //                             Text('Date: $formattedDate', style: TextStyle(fontFamily: 'Poppins',
-  //                                   fontStyle: FontStyle.normal,
-  //                                   fontWeight: FontWeight.w400,
-  //                                   fontSize: 12.0,
-  //                                   height: 1.5,),),
-  //                             Text('Time: $formattedTime', style: TextStyle(fontFamily: 'Poppins',
-  //                                   fontStyle: FontStyle.normal,
-  //                                   fontWeight: FontWeight.w400,
-  //                                   fontSize: 12.0,
-  //                                   height: 1.5,),),
-  //                   ],);
-  //                 },
-  //               ),
-  //           Stack(
-  //           children: [
-  //             Container(
-  //               height: 125,
-  //               decoration: const BoxDecoration(
-  //                 color: primaryOrange,
-  //                 borderRadius: BorderRadius.only(
-  //                   bottomLeft: Radius.circular(20.0),
-  //                   bottomRight: Radius.circular(20.0),
-  //                 ),
-  //               ),
-  //               padding: const EdgeInsets.only(bottom: 17),
-  //               child: Row(
-  //                 mainAxisAlignment: MainAxisAlignment.center,
-  //                 crossAxisAlignment: CrossAxisAlignment.end,
-  //                 children: const [
-  //                   Text(
-  //                     'STATUS',
-  //                     style: TextStyle(
-  //                       fontFamily: 'Poppins',
-  //                       fontStyle: FontStyle.normal,
-  //                       fontWeight: FontWeight.w600,
-  //                       fontSize: 24.0,
-  //                       height: 1.5,
-  //                       color: Colors.white,
-  //                     ),
-  //                   ),
-  //                 ],
-  //               ),
-  //             ),
-  //             Positioned(
-  //               left: 5,
-  //               top: 65,
-  //               child: ElevatedButton(
-  //                 onPressed: () {
-  //                   Navigator.of(context).pop();
-  //                 },
-  //                 style: ElevatedButton.styleFrom(
-  //                   backgroundColor: primaryOrange,
-  //                   shape: const CircleBorder(),
-  //                   padding: const EdgeInsets.fromLTRB(12, 4, 4, 4),
-  //                   fixedSize: const Size.fromRadius(25),
-  //                   elevation: 0,
-  //                 ),
-  //                 child: const Icon(Icons.arrow_back_ios, color: Colors.white),
-  //                   ),
-  //                 ),
-  //               ],
-  //             ),
-  //           ],
-            
-  //         );
-  //       },
-  //     );
-  //   }
-  //   );
-  // }
-  // )
